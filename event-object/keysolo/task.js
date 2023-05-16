aa96b82e5a040d1b1f5558bac2e45892198a802e
OlegJS777 <121798947+OlegJS777@users.noreply.github.com>
2023-05-16 20:06:49 +0300

    Add files via upload

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,14 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+     document.addEventListener("keydown", (event)=>{
+      let letter = event.key
+      if(this.currentSymbol.textContent === letter){
+        this.success();
+      }else{
+        this.fail()
+      }
+    })
   }
 
   success() {
