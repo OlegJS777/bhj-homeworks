8243e974b804f77fa64454e79989f868f8e214db
OlegJS777 <121798947+OlegJS777@users.noreply.github.com>
2023-05-16 20:21:36 +0300

    Add files via upload

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,13 +1,13 @@
-let tab =document.querySelectorAll(".tab");
+let tab = document.querySelectorAll(".tab");
 let tabContent = document.querySelectorAll(".tab__content")
 for(let i = 0; i< tab.length; i++){
-    let element = tab[i];
-    let cont = tabContent[i];
-    element.addEventListener('click',function(){
+    let tabDoc = tab[i];
+    let content = tabContent[i];
+    tabDoc.addEventListener('click',function(){
         document.querySelector(".tab_active").classList.remove("tab_active")
-        element.classList.add("tab_active");
-        document.querySelector(".tab__content_active").classList.remove("tab__content_active")
-        cont.classList.add("tab__content_active")
+            tabDoc.classList.add("tab_active");
+                document.querySelector(".tab__content_active").classList.remove("tab__content_active")
+                    content.classList.add("tab__content_active")
 
     })
 }
