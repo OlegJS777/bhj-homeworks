bee361aa3a98dacd23aa0be8201989903d9c8ece
OlegJS777 <121798947+OlegJS777@users.noreply.github.com>
2023-05-25 20:50:43 +0300

    Add files via upload

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,25 +68,21 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
+    let options = document.querySelector("select").options;
 
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
+    let results = [];
+
+    for(let i = 0; i<options.length; i++){
+      if(options[i].text.includes(text)){
+        let tmp = { 
+          text: options[i].text,
+          value: options[i].value
+        }
+        results.push(tmp);
       }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    }
+    
+    return results;
   }
 }
 
