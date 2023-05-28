b5f0440efaebd535e8c71b7bac59c8a4fdef9ff6
OlegJS777 <121798947+OlegJS777@users.noreply.github.com>
2023-05-28 18:52:07 +0300

    Add files via upload

---
+++ document-structure/todo/task.js
@@ -0,0 +1,32 @@
+let tasksAdd = document.getElementById("tasks__add");
+let input = document.querySelector("input");
+let tasksList = document.querySelector(".tasks__list");
+
+function addTask(event) {
+  event.preventDefault();
+    input.value = input.value.trim();
+if(input.value == "") {
+    return;
+  }
+
+tasksList.insertAdjacentHTML('afterbegin',`<div class="task">
+    <div class="task__title">
+        ${input.value}
+            </div>
+<a href="#" class="task__remove">&times;</a>
+</div>`);
+
+input.value = null;
+
+let deletetasksAdd = document.querySelector(".task__remove");
+  deletetasksAdd.addEventListener("click", () => {
+    deletetasksAdd.closest(".task").remove();
+  })
+}
+
+tasksAdd.onclick = addTask;
+    input.addEventListener("keydown", (item) => {
+        if(item.code == "Enter") {
+            addTask();
+  }
+})
