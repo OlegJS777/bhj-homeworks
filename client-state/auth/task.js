e380d21a9b1d19e4907abece1848deb637e2f687
OlegJS777 <121798947+OlegJS777@users.noreply.github.com>
2023-06-06 19:17:20 +0300

    Add files via upload

---
+++ client-state/auth/task.js
@@ -0,0 +1,39 @@
+let signinForm = document.getElementById("signin__form");
+let useId = document.getElementById("user_id");
+let welcome = document.getElementById("welcome");
+
+signinForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let form = new FormData(signinForm);
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/auth");
+  xhr.responseType = "json";
+  xhr.send(form);
+
+  xhr.onload = function () {
+    let object = xhr.response;
+
+    if (object.user_id) {
+        useId.textContent = object.user_id;
+      welcomeUser();
+      localStorage.setItem("id", useId.textContent);
+    } else {
+      alert("Неверный логин и/или пароль");
+    }
+  };
+  signinForm.reset();
+});
+window.onload = function () {
+  let useId = localStorage.getItem("id");
+
+  if (useId) {
+    useId.textContent = useId;
+    welcomeUser();
+  }
+};
+
+function welcomeUser() {
+    signinForm.classList.add("welcome");
+  welcome.classList.add("welcome_active");
+}
